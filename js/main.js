e650b6fe590626195e5e6413b21f5383b62694b4
Nancy  Nguyen <Prancy@Nancys-Air.home>
2017-09-24 00:01:07 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+cards.push('cardOne');
+console.log("User flipped queen");
+var cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
+if (cardsInPlay.length === 2) {
+	console.log("you have selected two cards");
+}
+if (cardsInPlay[0] === cards.length[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again");
+}
